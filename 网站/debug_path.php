b1746cdad322b7829c8b58e1548d7afb630a188c
覃金融 <jinqin@users.noreply.gitee.com>
2026-07-21 01:29:18 +0000

    chore: 移除调试文件

--- 网站/debug_path.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo 'SCRIPT_FILENAME: ' . ($_SERVER['SCRIPT_FILENAME'] ?? 'NOT SET') . "\n";
-echo 'APP_PATH: ' . (dirname($_SERVER['SCRIPT_FILENAME']) . DIRECTORY_SEPARATOR) . "\n";
-echo 'ROOT_PATH: ' . (dirname(realpath(dirname($_SERVER['SCRIPT_FILENAME']) . DIRECTORY_SEPARATOR)) . DIRECTORY_SEPARATOR) . "\n";
--- 网站/test163.php
+++
@@ -1,7 +0,0 @@
-<?php
-$url = $_POST['url'] ?? ($_GET['url'] ?? '');
-if (empty($url)) die('no url');
-$cmd = 'php8.2 /workspace/website/网站/api_parser/parse.php ' . escapeshellarg($url) . ' 2>/dev/null';
-$data = shell_exec($cmd);
-header('Content-Type: application/json');
-echo $data;
